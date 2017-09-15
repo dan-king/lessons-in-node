d9f1b24b020b6e9d9645caebe603e2106f92cadb
Dan King <dan.king@viewpoint.pro>
2017-09-15 15:49:35 -0700

    Added Handlebars template views

--- lesson-05/README.md
+++ lesson-05/README.md
@@ -12,9 +12,22 @@ Install the Express Handlebars view engine and add add it to your package.json f
 npm install --save express-handlebars
 ````
 
-A shortcut to the syntax above is
+A shortcut to the syntax above is:
 
 ````bash
 npm i -S express-handlebars
 ````
 
+In addition to the text specfic to each page, the links below will also include a navigation header and copyright footer.
+
+Browsing to [http://localhost:3000/](http://localhost:3000/) will display 
+
+	Home Sweet Home!
+
+Broswing to [http://localhost:3000/about](http://localhost:3000/about) will display 
+
+	This is the about page.
+
+Browsing to any other page, such as [http://localhost:3000/bogus](http://localhost:3000/bogus), will display 
+
+	404 - Not Found
--- lesson-05/lesson-05.js
+++ lesson-05/lesson-05.js
@@ -1,1 +1,49 @@
-//lesson-05.js
+var express = require('express');
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+//
+// Define routes
+//
+
+// Home
+app.get('/', function(req, res){
+        res.render('home');
+});
+
+// About
+app.get('/about', function(req, res){
+        res.render('about');
+});
+
+//
+// Special routes if page not found or an error occured
+//
+
+// custom 404 page
+app.use(function(req, res){
+        res.status(404);
+        res.render('404');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+        console.error(err.stack);
+        res.status(500);
+        res.render('500');
+});
+
+//
+// Listen on the given port
+//
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
